a166a4b7e565bce4b8e29a1de9ff3630366cea72
Cavan Stewart <cavanwstewart@gmail.com>
2017-02-01 23:52:56 -0800

    initial commit

---
+++ blackjack.cpp
@@ -0,0 +1,112 @@
+#include "blackjack.h"
+
+//selects a random card
+Card::Card(){
+	int r = 1 + rand() % 4;
+	switch (r) {
+	case 1: suit = OROS; break;
+	case 2: suit = COPAS; break;
+	case 3: suit = ESPADAS; break;
+	case 4: suit = BASTOS; break;
+	default: break;
+	}
+
+	r = 1 + rand() % 10;
+	switch (r) {
+	case  1: rank = AS; break;
+	case  2: rank = DOS; break;
+	case  3: rank = TRES; break;
+	case  4: rank = CUATRO; break;
+	case  5: rank = CINCO; break;
+	case  6: rank = SEIS; break;
+	case  7: rank = SIETE; break;
+	case  8: rank = SOTA; break;
+	case  9: rank = CABALLO; break;
+	case 10: rank = REY; break;
+	default: break;
+	}
+}
+
+// Accessor: returns a string with the suit of the card in Spanish 
+string Card::get_spanish_suit() const {
+	string suitName;
+	switch (suit) {
+	case OROS:
+		suitName = "oros";
+		break;
+	case COPAS:
+		suitName = "copas";
+		break;
+	case ESPADAS:
+		suitName = "espadas";
+		break;
+	case BASTOS:
+		suitName = "bastos";
+		break;
+	default: break;
+	}
+	return suitName;
+}
+
+// Accessor: returns a string with the rank of the card in Spanish 
+string Card::get_spanish_rank() const {
+	string rankName;
+	switch (rank) {
+	case AS:
+		rankName = "As";
+		break;
+	case DOS:
+		rankName = "Dos";
+		break;
+	case TRES:
+		rankName = "Tres";
+		break;
+	case CUATRO:
+		rankName = "Cuatro";
+		break;
+	case CINCO:
+		rankName = "Cinco";
+		break;
+	case SEIS:
+		rankName = "Seis";
+		break;
+	case SIETE:
+		rankName = "Siete";
+		break;
+	case SOTA:
+		rankName = "Sota";
+		break;
+	case CABALLO:
+		rankName = "Caballo";
+		break;
+	case REY:
+		rankName = "Rey";
+		break;
+	default: break;
+	}
+	return rankName;
+}
+
+
+
+// Accessor: returns a string with the suit of the card in English 
+string Card::get_english_suit() const {
+	return "";
+}
+
+// Accessor: returns a string with the rank of the card in English 
+string Card::get_english_rank() const {
+	return "";
+}
+
+
+// Assigns a numerical value to card based on rank.
+int Card::get_rank() const {
+	return static_cast<int>(rank)+1;
+}
+
+// Comparison operator for cards
+bool Card::operator < (Card card) const {
+	return rank < card.rank;
+}
+
---
+++ blackjack.h
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <fstream>
+
+using std::string;
+#ifndef CARDS_H
+#define CARDS_H
+
+enum suit_t { OROS, COPAS, ESPADAS, BASTOS };
+
+enum rank_t { AS, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, SOTA = 9, CABALLO = 10, REY = 11 };
+
+
+class Card{
+public:
+	Card();
+
+	//Accessors
+	string get_spanish_suit() const;
+	string get_spanish_rank() const;
+
+	string get_english_suit() const;
+	string get_english_rank() const;
+
+	int get_rank() const;
+
+	bool operator < (Card card) const;
+private:
+	suit_t suit;
+	rank_t rank;
+};
+
+class Hand {
+public:
+	Hand();
+private:
+};
+
+class Player{
+public:
+	Player(int m);
+
+private:
+	int money;
+};
+
+
+
+
+#endif
